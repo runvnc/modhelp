b0a3509fcbb06b5d6fb38a08694aaa77fb0af510
Jason Livesay <ithkuil@gmail.com>
2015-01-16 03:58:04 +0000

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ marked.setOptions({
 
 //var mod = process.argv[2];
 mod='chalk';
-glob('node_modules/'+mod+'**/*.md', null, function(er, files) {
+glob('node_modules/'+mod+'/*.md', null, function(er, files) {
   if (files && files.length && files.length>0) {
     var readme = fs.readFileSync(files[0], 'utf8');
     console.log(marked(readme));
