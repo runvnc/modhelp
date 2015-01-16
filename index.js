123452432410933d73a7704c3748fe74dd8c8d9c
Jason Livesay <ithkuil@gmail.com>
2015-01-16 03:42:24 +0000

    uses first md file now

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 var marked = require('marked'),
     fs = require('fs'),
+    glob = require('glob'),
     TerminalRenderer = require('marked-terminal');
 
 marked.setOptions({ 
   renderer: new TerminalRenderer() 
 }); 
 
-var mod = process.argv[2];
+//var mod = process.argv[2];
+mod='chalk';
+glob('node_modules/'+mod+'**/*.md', null, function(er, files) {
+  if (files && files.length && files.length>0) {
+    var readme = fs.readFileSync(files[0], 'utf8');
+    console.log(marked(readme));
+  }
+});
 
-var readme = fs.readFileSync('node_modules/' + mod + '/README.md', 'utf8');
-
-console.log(marked(readme));
